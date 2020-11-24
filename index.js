289bc6ccbb390282e2b33af6633d8c0fa5e8d33c
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-11-24 16:15:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('input');
+const ul = document.querySelectorAll('ul')[0];
+const li = document.querySelectorAll('li')[1];
+
+input.addEventListener('focus', () => {
+    input.classList.add('input-grow')
+    ul.classList.add('input-grow');
+    li.classList.add('input-grow');
+});
+
+input.addEventListener('focusout', () => {
+    input.classList.remove('input-grow')
+    ul.classList.remove('input-grow');
+    li.classList.remove('input-grow');
+});
