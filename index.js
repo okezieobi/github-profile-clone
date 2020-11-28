c1cfdef6f25b2d7e809bdc170b8ccb38a0c6ad09
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-11-28 16:21:06 +0100

    6th commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ window.addEventListener('load', () => {
 const input = document.querySelector('[header-input]');
 const ul = document.querySelector('[header-input-list]');
 const li = document.querySelector('[header-input-item]');
+const mainDropdown = document.querySelector('[drop-list]');
+const headerMenu = document.querySelector('[header-menu-icon]');
 
 input.addEventListener('focus', () => {
     input.classList.add('input-grow')
@@ -14,4 +16,9 @@ input.addEventListener('focusout', () => {
     ul.classList.remove('input-grow');
     li.classList.remove('input-grow');
 });
+    
+    headerMenu.addEventListener('click', () => {
+        if (mainDropdown.classList.contains('hide-element')) mainDropdown.classList.remove('hide-element');
+        else mainDropdown.classList.add('hide-element');
+})
 })
