4104d1872ba05790c102a1c5c7ee9e8e7aa4d21a
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-11-30 01:45:07 +0100

    11th commit

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ window.addEventListener('load', () => {
             {
                 viewer { 
                     login
-                   avatarUrl(size: 20) 
+                   avatarUrl(size: 300) 
                     }
             }
             `;
